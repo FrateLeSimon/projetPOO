983d8a0d66965baae9f75123e006c5f6a939e96e
Bastien <bastien.zagala@viacesi.fr>
2022-12-01 10:14:44 +0100

    Ajout des constructeur

--- Facture.cpp
+++ Facture.cpp
@@ -1,6 +1,20 @@
 #include "tools.h"
 #include "Facture.h"
 
+Facture::Facture()
+{
+	id_facture = "";
+	nom_societe = "";
+	num_rue_societe = "";
+	nom_rue_societe = "";
+	ville_societe = "";
+	code_postal_societe = 0;
+	logo_societe = "";
+	nbr_paiement = 0;
+	somme_paiements = 0;
+	moyen = "";
+}
+
 void Facture::setId_Facture(string a)
 {
 
