316cd22f429690c2c9117173e4556e3cda4d6c0a
FrateLeSimon <simon.gulden121203@gmail.com>
2022-12-01 11:53:20 +0100

    Form cpp

--- projetPOO/ConnexionForm.cpp
+++ projetPOO/ConnexionForm.cpp
@@ -1,2 +1,12 @@
 #include "ConnexionForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int __clrcall WinMain(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    projetPOO::ConnexionForm monFormulaire;
+    Application::Run(% monFormulaire);
+}
