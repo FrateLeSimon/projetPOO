98e14d3dfe0d26b0028ba4c5ee026a6c02eb3056
Bastien <bastien.zagala@viacesi.fr>
2022-12-02 09:47:11 +0100

    AJout de return fictifs

--- Mappage.cpp
+++ Mappage.cpp
@@ -3,21 +3,21 @@
 
 string Mappage::Insert()
 {
-
+	return "Insert";
 }
 
 string Mappage::Select()
 {
-
+	return "Select";
 }
 
 string Mappage::Update()
 {
-
+	return "Update";
 }
 
 string Mappage::Delete()
 {
-
+	return "Delete";
 }
 
