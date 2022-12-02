ea1bad327b44e3e39d1dbf080ac72b851f505758
Bastien <bastien.zagala@viacesi.fr>
2022-12-02 14:32:40 +0100

    Modif main

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 int main()
 {
-	cout << "Initaialisation...";
 	Client client(01, "John", "Smith", "1a", "5c", "Rue de Paris", "Boulevard de Napoléon", "Lyon", "Paris", 69000, 75000, "01/01/1998", "12/11/2022");
 	client.getId_Client();
 }
