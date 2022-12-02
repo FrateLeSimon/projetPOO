e04aa3fd714d6ddac61035567d6b6fec28074930
FrateLeSimon <simon.gulden121203@gmail.com>
2022-12-02 14:30:23 +0100

    cad cpp

---
+++ CAD.cpp
@@ -0,0 +1,36 @@
+#include "tools.h"
+#include "CAD.h"
+
+
+NS_Comp_Data::CLcad::CLcad(void)
+{
+	this->sCnx = "Data Source=PC-ARNAUD\\MSSQL_CPI;User ID=CNX_P6;Password=8BkrG%@Jwgk8na@*N^X$Av8Gt^8qCsf6$W;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+	this->sSql = "Rien";
+
+	this->oCnx = gcnew System::Data::SqlClient::SqlConnection(this->sCnx);
+	this->oCmd = gcnew System::Data::SqlClient::SqlCommand(this->sSql, this->oCnx);
+	this->oDA = gcnew System::Data::SqlClient::SqlDataAdapter();
+	this->oDs = gcnew System::Data::DataSet();
+
+	this->oCmd->CommandType = System::Data::CommandType::Text;
+}
+System::Data::DataSet^ NS_Comp_Data::CLcad::getRows(System::String^ sSql, System::String^ sDataTableName)
+{
+	this->oDs->Clear();
+	this->sSql = sSql;
+	this->oCmd->CommandText = this->sSql;
+	this->oDA->SelectCommand = this->oCmd;
+	this->oDA->Fill(this->oDs, sDataTableName);
+
+	return this->oDs;
+}
+void NS_Comp_Data::CLcad::actionRows(System::String^ sSql)
+{
+	this->sSql = sSql;
+	this->oCmd->CommandText = this->sSql;
+	this->oDA->SelectCommand = this->oCmd;
+	this->oCnx->Open();
+	this->oCmd->ExecuteNonQuery();
+	this->oCnx->Close();
+}
