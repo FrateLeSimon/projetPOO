415d8bdd53ecab706d2507f80bca071a290cb8a8
Bastien <bastien.zagala@viacesi.fr>
2022-12-02 14:26:23 +0100

    Ajout de la méthode CAD

---
+++ projetPOO/CAD.cpp
@@ -0,0 +1,35 @@
+#include "tools.h"
+#include "CAD.h"
+
+NS_Comp_Data::CLcad::CLcad()
+{
+	this->sCnx = "Data Source = 34.155.198.90;Initial Catalog = BDD-Projet-POO;User ID = default; Password=ProjetPOO";
+
+	this->sSql = "Rien";
+
+	this->oCnx = gcnew System::Data::SqlClient::SqlConnection(this->sCnx);
+	this->oCmd = gcnew System::Data::SqlClient::SqlCommand(this->sSql, this->oCnx);
+	this->oDA = gcnew System::Data::SqlClient::SqlDataAdapter();
+	this->oDs = gcnew System::Data::DataSet();
+
+	this->oCmd->CommandType = System::Data::CommandType::Text;
+}
+System::Data::DataSet^ NS_Comp_Data::CLcad::getRows(System::String^ sSql, System::String^ sDataTableName)
+{
+	this->oDs->Clear();
+	this->sSql = sSql;
+	this->oCmd->CommandText = this->sSql;
+	this->oDA->SelectCommand = this->oCmd;
+	this->oDA->Fill(this->oDs, sDataTableName);
+
+	return this->oDs;
+}
+void NS_Comp_Data::CLcad::actionRows(System::String^ sSql)
+{
+	this->sSql = sSql;
+	this->oCmd->CommandText = this->sSql;
+	this->oDA->SelectCommand = this->oCmd;
+	this->oCnx->Open();
+	this->oCmd->ExecuteNonQuery();
+	this->oCnx->Close();
+}
