76ecb1e24c6021785f57aa43ee36e91477dc5ca5
ArthurStrzz <arthur.strazzeri@viacesi.fr>
2022-12-04 11:23:17 +0100

    rzg

--- projetPOO/GestionClientForm.h
+++ projetPOO/GestionClientForm.h
@@ -34,8 +34,8 @@ namespace projetPOO {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
+
+
 	protected:
 
 	private:
@@ -51,50 +51,21 @@ namespace projetPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(29, 21);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
-			this->label1->Click += gcnew System::EventHandler(this, &GestionClientForm::label1_Click);
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(276, 227);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
-			this->label2->TabIndex = 1;
-			this->label2->Text = L"label2";
-			this->label2->Click += gcnew System::EventHandler(this, &GestionClientForm::label2_Click);
 			// 
 			// GestionClientForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(834, 353);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
 			this->Name = L"GestionClientForm";
 			this->Text = L"GestionClientForm";
 			this->Load += gcnew System::EventHandler(this, &GestionClientForm::GestionClientForm_Load);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void GestionClientForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	};
 }
