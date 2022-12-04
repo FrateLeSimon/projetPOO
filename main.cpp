8c86abcbf08e63e851b8871b69f17ccc847882d1
FrateLeSimon <simon@PC-SIMON>
2022-12-04 15:39:37 +0100

    plus de bug

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#include "CL.h"
 #include "tools.h"
 
 int main()
 {
-	Client client(01, "John", "Smith", "1a", "5c", "Rue de Paris", "Boulevard de Napoléon", "Lyon", "Paris", 69000, 75000, "01/01/1998", "12/11/2022");
-	client.getId_Client();
+
 }
