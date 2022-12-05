d09e5f0f82ba5fa0a262fe93007b4f51bb5bcb2a
ArthurStrzz <arthur.strazzeri@viacesi.fr>
2022-12-05 09:59:44 +0100

    stat II

--- projetPOO/GestionStatForm.h
+++ projetPOO/GestionStatForm.h
@@ -48,11 +48,17 @@ namespace projetPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"GestionStatForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// GestionStatForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(751, 337);
+			this->Name = L"GestionStatForm";
+			this->Text = L"GestionStatForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
