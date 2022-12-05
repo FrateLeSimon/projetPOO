b70d82e4c40208144081be0caa4a7c1fc5ab0d30
Bastien <bastien.zagala@viacesi.fr>
2022-12-05 14:26:35 +0100

    Correction d'article

--- requetes sql.sql
+++ requetes sql.sql
@@ -1,6 +1,6 @@
 SELECT prenom, nom, a1.num_rue AS num_rue_livraison, a1.nom_rue AS nom_rue_livraison, v1.code_postal AS code_postal_livraison, v1.nom_ville AS ville_livraison, 
 a2.num_rue AS num_rue_facturation, a2.nom_rue AS nom_rue_facturation, v2.code_postal AS code_postal_facturation, v2.nom_ville AS ville_facturation, cal1.c_date AS date_naissance, d.c_date AS date_premier_achat
-FROM client AS c WHERE prenom = "Dora" 
+FROM client AS c 
 INNER JOIN humain AS h ON c.id_humain = h.id_humain 
 INNER JOIN adresse AS a1 ON c.id_adresse_livraison = a1.id_adresse 
 INNER JOIN ville AS v1 ON a1.id_ville = v1.id_ville
@@ -10,7 +10,6 @@ INNER JOIN calendrier AS cal1 ON c.id_date_naissance = cal1.id_calendrier
 left JOIN (SELECT TOP 100 co.id_client, cal2.c_date FROM commande AS co 
             INNER JOIN calendrier AS cal2 ON co.id_date_emission = cal2.id_calendrier
             ORDER BY cal2.c_date ) AS d ON c.id_client = d.id_client
-            /*WHERE prenom = 'Dora'*/
 
 SELECT c.id_commande AS "Référence/Commande", c.id_facture AS Facture, h.nom as "Nom", prenom as Prenom, d1.c_date AS "Date de livraison", d2.c_date AS "Date d'émission", art.nom AS "Article", ct.quantite AS "Quantité", f.montant_total_ht AS "Montant HT"
 FROM commande AS c 
@@ -29,6 +28,7 @@ INNER JOIN calendrier AS c ON p.id_calendrier = c.id_calendrier
 INNER JOIN adresse AS a ON p.id_adresse = a.id_adresse
 INNER JOIN ville AS v ON a.id_ville = v.id_ville
 
-SELECT nom AS "Nom artcile", couleur AS "Couleur", quantite_stock AS "Stock" 
+SELECT nom AS "Nom artcile", couleur AS "Couleur", prix_article_ht AS "Prix (€)", seuil AS "Seuil", t.taxe AS "Taxe (%)", quantite_stock AS "Stock" 
 FROM article
+INNER JOIN taxe AS t ON article.id_taxe = t.id_taxe
 ORDER BY quantite_stock DESC
